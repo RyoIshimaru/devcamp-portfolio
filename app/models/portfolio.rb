7a5582d47314d4ea097e0d07f67aedfea4c059e4
RyoIshimaru <ryo.ishimaru.kyoto@gmail.com>
2020-02-25 20:43:46 -0800

    Implemented default image to portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,4 +7,10 @@ class Portfolio < ApplicationRecord
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails")}
 
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "https://via.placeholder.com/600x240.png?%20C/O%20https://placeholder.com/"
+    self.thumb_image ||= "https://via.placeholder.com/300x120.png?%20C/O%20https://placeholder.com/"
+  end
 end
